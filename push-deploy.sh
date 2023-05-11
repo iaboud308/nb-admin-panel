373db813fcca271c31eb2e38f417fd819c24c087
Ibrahim <iaboud308@gmail.com>
2023-05-11 19:36:45 +0100

    ..

--- push-deploy.sh
+++ push-deploy.sh
@@ -1,11 +1,30 @@
 
 #! /bin/bash
 
+npm run build
 
-# git add .
-# git commit -m '..'
-# git push
+git add .
+git commit -m '..'
+git push
 
-# npm run build
-# cp -r build /Users/ibrahim/Documents/Software/Helpers/nginx-docker/react-build/nb-admin-panel/
-/Users/ibrahim/Documents/Software/Helpers/nginx-docker/push-deploy.sh
+
+ssh -T hyderion@hyderion.com <<-END
+
+cd software/mm/ui
+
+git pull
+npm run build
+cp -r build /home/hyderion/nginx-docker/react-build/nb-admi-panel
+docker exec nginx-c nginx -s reload
+
+# sudo systemctl restart nginx.service
+
+exit
+
+END
+
+echo "All done"
+
+
+
+
